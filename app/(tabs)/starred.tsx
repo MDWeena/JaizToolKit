3702c424eb33898a1d2cef4759afea2f91e4526b
Winner Akpologun <akpologun.winner@gmail.com>
2025-10-25 08:37:46 +0100

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind and tailwindcss to package.json
    - Updated react-native-reanimated and react-native-safe-area-context versions
    - Created tailwind.config.js for Tailwind CSS setup
    - Updated tsconfig.json to include nativewind-env.d.ts

---
+++ app/(tabs)/starred.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const starred = () => {
+  return (
+    <View>
+      <Text>starred</Text>
+    </View>
+  )
+}
+
+export default starred
